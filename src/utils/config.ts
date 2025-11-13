714dcdec7ec00df55fc583abd39d9a00f5c5cc58
Joxeph <codewithjoxeph@gmail.com>
2025-11-13 20:04:47 +0100

    fix: image fix

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,12 +1,14 @@
 import image1 from "@/assets/building.jpg"
 import image2 from "@/assets/image.jpg"
-import image3 from "@/assets/building.jpg"
+import image3 from "@/assets/applicant.jpg"
 
 export enum Config {
   domain = "https://shot-count.vercel.app/",
   description = "ShotCount helps you take more shots, learn from rejection, and grow your confidence with AI-powered insights.",
   title = "ShotCount",
 }
+
+
 
 export const APPCONFIG = {
   APP_URL: {
